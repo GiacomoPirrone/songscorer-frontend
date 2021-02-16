f3cf34b4824226f4ef25d2683e06715a852106f3
GiacomoLPirrone <giacomollpirrone@gmail.com>
2021-02-16 21:56:29 +1100

    Basic header setup complete

---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+
+import {Form, Container, Row, Col, Image, Button} from "react-bootstrap";
+
+
+const Login = props => (
+    <>
+    </>
+)
+export default Login;
